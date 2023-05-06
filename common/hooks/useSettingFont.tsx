886fbe772cc3ba9f19a3382bff296875bed66248
ww8007 <wshmin1234@gmail.com>
2023-05-06 23:02:14 +0900

    feat: 폰트 설정하는 custom hook 추가

---
+++ common/hooks/useSettingFont.tsx
@@ -0,0 +1,17 @@
+import { useFonts } from 'expo-font';
+import React, { useEffect } from 'react';
+
+const useSettingFont = () => {
+  const [loaded, error] = useFonts({
+    NanumSquareRound: require('../../assets/fonts/NanumSquareRoundR.ttf'),
+    NanumSquareBold: require('../../assets/fonts/NanumSquareRoundB.ttf'),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  return { loaded };
+};
+
+export default useSettingFont;
