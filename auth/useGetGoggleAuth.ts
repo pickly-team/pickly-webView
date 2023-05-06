75deec200ea42f4e22daee70909c7c51fc78d165
ww8007 <wshmin1234@gmail.com>
2023-05-06 23:01:34 +0900

    feat: Goggle oauth 로그인 로직 추가

---
+++ auth/useGetGoggleAuth.ts
@@ -0,0 +1,47 @@
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithCredential,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
+WebBrowser.maybeCompleteAuthSession();
+
+import { useEffect } from 'react';
+
+const useGetGoogleAuth = () => {
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+  });
+  const auth = getAuth();
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential);
+    }
+  }, [response]);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const { uid, email, displayName, refreshToken } = user;
+        console.log('uid :', uid);
+        console.log('email :', email);
+        console.log('displayName :', displayName);
+        console.log('refreshToken :', refreshToken);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [auth]);
+
+  return { request, response, promptAsync };
+};
+
+export default useGetGoogleAuth;
