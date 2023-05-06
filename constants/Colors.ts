64c24b2c3b657e0ffacb82efd8a240f1e4de52a6
ww8007 <wshmin1234@gmail.com>
2023-05-06 22:56:50 +0900

    feat: 피클리 기본 색상 코드 추가

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,41 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+	light: {
+		text: "#fff",
+		background: "#161617",
+		primary: "#76B100",
+		lightPrimary: "#A7E100",
+		darkPrimary: "#8EC520",
+		lightGreen: "#BBFFB5",
+		buttonGreen: "#10AB10",
+		white: "#ffffff",
+		black: "#161617",
+		grey200: "#EEEEEE",
+		grey400: "#BDBDBD",
+		grey600: "#757575",
+		grey700: "#616161",
+		grey800: "#424242",
+		grey900: "#212121",
+		darkBlack: "#161514",
+		darkGrey: "#2A2A2A"
+	},
+	dark: {
+		text: "#fff",
+		background: "#161617",
+		primary: "#76B100",
+		lightPrimary: "#A7E100",
+		darkPrimary: "#8EC520",
+		lightGreen: "#BBFFB5",
+		buttonGreen: "#10AB10",
+		white: "#ffffff",
+		black: "#161617",
+		grey200: "#EEEEEE",
+		grey400: "#BDBDBD",
+		grey600: "#757575",
+		grey700: "#616161",
+		grey800: "#424242",
+		grey900: "#212121",
+		darkBlack: "#161514",
+		darkGrey: "#2A2A2A",
+		google: "#5D5FEF"
+	}
 };
