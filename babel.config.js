bc8e4339f712cba21dda8a2ebe212120ad7cc196
ww8007 <wshmin1234@gmail.com>
2023-06-18 06:04:46 +0900

    fix: transform-inline-environment-variables 제거

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
 module.exports = function (api) {
-	api.cache(true);
-	return {
-		presets: ["babel-preset-expo"],
-		plugins: [
-			require.resolve("expo-router/babel"),
-			"transform-inline-environment-variables"
-		]
-	};
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [require.resolve('expo-router/babel')],
+  };
 };
