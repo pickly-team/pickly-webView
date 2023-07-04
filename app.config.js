ae3ba464b4e7136ad41e09761158e0cc0f8cc815
ww8007 <wshmin1234@gmail.com>
2023-07-04 18:07:56 +0900

    build: env 사용을 위한 js 방식으로 변경 및 extra 필드 추가

---
+++ app.config.js
@@ -0,0 +1,51 @@
+require('dotenv/config');
+
+export default {
+  expo: {
+    name: 'pickly-app',
+    slug: 'pickly',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'pickly-app',
+    userInterfaceStyle: 'automatic',
+    splash: {
+      image: './assets/images/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.ww8007.pickly',
+      buildNumber: '4',
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+    },
+    web: {
+      bundler: 'metro',
+      favicon: './assets/images/favicon.png',
+    },
+    extra: {
+      eas: {
+        projectId: '2ff1c8d5-ce1d-47d7-86d7-ebe5d015089a',
+      },
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID,
+      expoClientId: process.env.EXPO_CLIENT_ID,
+      iosClientId: process.env.IOS_CLIENT_ID,
+      androidClientId: process.env.ANDROID_CLIENT_ID,
+    },
+    plugins: ['expo-apple-authentication'],
+    owner: 'ww8007',
+  },
+};
