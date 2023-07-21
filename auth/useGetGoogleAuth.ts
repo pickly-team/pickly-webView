06f0ae55d2eb0546363972b15c733a900002ba84
ww8007 <wshmin1234@gmail.com>
2023-07-21 22:20:56 +0900

    fix: native firebase 사용에 따라 구글 로그인 로직 수정

--- auth/useGetGoogleAuth.ts
+++ auth/useGetGoogleAuth.ts
@@ -1,48 +1,25 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as Google from 'expo-auth-session/providers/google';
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import Constants from 'expo-constants';
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithCredential,
-  onAuthStateChanged,
-} from 'firebase/auth';
-import { useEffect } from 'react';
-
-WebBrowser.maybeCompleteAuthSession();
+GoogleSignin.configure({
+  webClientId: Constants.expoConfig?.extra?.expoClientId || '',
+});
 
 const useGetGoogleAuth = () => {
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    expoClientId: Constants.expoConfig?.extra?.expoClientId || '',
-    iosClientId: Constants.expoConfig?.extra?.iosClientId || '',
-    androidClientId: Constants.expoConfig?.extra?.androidClientId || '',
-  });
-  const auth = getAuth();
+  const onClickGoogleLogin = async () => {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
 
-  useEffect(() => {
-    if (response?.type === 'success') {
-      const { id_token } = response.params;
-      const credential = GoogleAuthProvider.credential(id_token);
-      signInWithCredential(auth, credential);
-    }
-  }, [response]);
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        const { uid, email, displayName, refreshToken } = user;
-        console.log('uid :', uid);
-        console.log('email :', email);
-        console.log('displayName :', displayName);
-        console.log('refreshToken :', refreshToken);
-      }
-    });
-    return () => {
-      unsubscribe();
-    };
-  }, [auth]);
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  };
 
-  return { request, response, promptAsync };
+  return { onClickGoogleLogin };
 };
 
 export default useGetGoogleAuth;
