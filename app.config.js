7b633e09acff18dfa41a2b325bfa68cc3b5b7a1d
ww8007 <wshmin1234@gmail.com>
2023-07-23 05:10:14 +0900

    build: 빌드 넘버 11로 수정

--- app.config.js
+++ app.config.js
@@ -26,7 +26,7 @@ export default {
       googleServicesFile: './ios/GoogleService-Info.plist',
       supportsTablet: true,
       bundleIdentifier: 'com.ww8007.pickly',
-      buildNumber: '10',
+      buildNumber: '11',
       infoPlist: {
         UIBackgroundModes: ['remote-notification'],
       },
