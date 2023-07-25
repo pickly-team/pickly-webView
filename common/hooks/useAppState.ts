a8cb0a4e92dc5e53d926eed57ef06e564fcf09f0
ww8007 <wshmin1234@gmail.com>
2023-07-25 20:43:52 +0900

    feat: 앱의 상태를 체크하는 useAppState 커스텀 훅 추가

---
+++ common/hooks/useAppState.ts
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { AppState, AppStateStatus } from 'react-native';
+
+const useAppState = () => {
+  const appState = useRef(AppState.currentState);
+  const [activeAppState, setActiveAppState] = useState(appState.current);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener(
+      'change',
+      handleAppStateChange,
+    );
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const handleAppStateChange = (nextAppState: AppStateStatus) => {
+    if (
+      appState.current.match(/inactive|background/) &&
+      nextAppState === 'active'
+    ) {
+    }
+    appState.current = nextAppState;
+    setActiveAppState(appState.current);
+  };
+
+  return { activeAppState };
+};
+
+export default useAppState;
