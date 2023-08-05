d02dd3305901ac7e6420f0a291f9c751a749db98
ww8007 <wshmin1234@gmail.com>
2023-08-05 20:42:18 +0900

    fix: webview -> initializeWebview 워딩 수정

--- auth/api/login.ts
+++ auth/api/login.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
 import Constants from 'expo-constants';
 import client from '../../lib/client';
-import { MODE } from '../../app/webview';
+import { MODE } from '../../app/initialWebview';
 export interface Json {
   email: string;
   isHardMode: boolean;
@@ -104,6 +104,7 @@ export const useGETMemberInfo = (params: GETUserInfoQueryParams) => {
       enabled: params.loginId !== 0,
       onSuccess: (data) => {
         if (data.nickname) params.setMode('SIGN_IN');
+        console.log('data:', data);
       },
       onError: (error) => {
         console.log('error:', error);
