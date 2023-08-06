0619c3cffa50d42a79a94e306cb5d37d281b8c5d
ww8007 <wshmin1234@gmail.com>
2023-08-07 03:27:14 +0900

    build: production에서 console 나오지 않도록 처리

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [require.resolve('expo-router/babel')],
+    plugins: [
+      require.resolve('expo-router/babel'),
+      'react-native-reanimated/plugin',
+    ],
+    env: {
+      production: {
+        plugins: ['transform-remove-console'],
+      },
+    },
   };
 };
