dfbd5f392ac6d3b2a39dd307f94a56eb25655d34
ww8007 <wshmin1234@gmail.com>
2023-08-09 08:32:14 +0900

    feat: 사용자 북마크 페이지 넘어가는 로직 추가 fix: postMessage 방식 변경

--- app/initialWebview.tsx
+++ app/initialWebview.tsx
@@ -29,6 +29,28 @@ import useNotification from '../notification/hooks/useNotification';
 import { captureScreen } from 'react-native-view-shot';
 import { useRouter } from 'expo-router';
 import auth from '@react-native-firebase/auth';
+import webviewStore from '../common/state/webview';
+import { navigationRef } from './_layout';
+
+export interface PostBridgeParams {
+  /** 웹뷰 로그인 */
+  login: null;
+  /** 알림 */
+  notification: null;
+  /** 페이지 뒤로 가기 */
+  goBack: null;
+  /** 회원가입 */
+  signUp: null;
+  /** 북마크 방문 */
+  visitBookmark: {
+    url: string;
+  };
+}
+
+interface WebviewOnMessage {
+  message: keyof PostBridgeParams;
+  params: PostBridgeParams[keyof PostBridgeParams];
+}
 
 export type MODE = 'SIGN_IN' | 'SIGN_UP';
 const clientUrl = Constants.expoConfig?.extra?.clientUrl || '';
@@ -50,6 +72,7 @@ const App = () => {
   const [snapshotUri, setSnapshotUri] = useState<string>('');
   const [loading, setLoading] = useState(true);
   const [isGoingBack, setIsGoingBack] = useState(false); // 뒤로 가기 상태 관리
+  const [noAnimation, setNoAnimation] = useState(false);
 
   const animationValue = useRef(new Animated.Value(windowWidth)).current;
   const snapShotAnimationValue = useRef(new Animated.Value(0)).current;
@@ -75,12 +98,14 @@ const App = () => {
   const handleNavigationStateChange = async (navState: WebViewNavigation) => {
     const url = navState.url;
     if (!isGoingBack) captureScreenFn(setSnapshotUri);
+    if (noAnimation) return;
+
     if (EXPLICIT_URL.includes(url) && !isGoingBack) {
       if (isInitialized) return;
       requestAnimationFrame(() => {
         Animated.timing(animationValue, {
           toValue: 0,
-          duration: 300,
+          duration: 250,
           easing: Easing.linear,
           useNativeDriver: true,
         }).start(() => {
@@ -97,7 +122,7 @@ const App = () => {
       requestAnimationFrame(() => {
         Animated.timing(animationValue, {
           toValue: 0,
-          duration: 300,
+          duration: 250,
           easing: Easing.linear,
           useNativeDriver: true,
         }).start(() => {
@@ -116,7 +141,7 @@ const App = () => {
     requestAnimationFrame(() => {
       Animated.timing(snapShotAnimationValue, {
         toValue: windowWidth,
-        duration: 300,
+        duration: 250,
         useNativeDriver: true,
         easing: Easing.linear,
       }).start(() => {
@@ -132,8 +157,10 @@ const App = () => {
 
   // 4. 웹뷰 메시지
   const router = useRouter();
+  const { setMode: setWebviewMode, setUrl } = webviewStore();
   const onWebViewMessage = (event: WebViewMessageEvent) => {
-    if (event.nativeEvent.data === 'login') {
+    const data = JSON.parse(event.nativeEvent.data) as WebviewOnMessage;
+    if (data.message === 'login') {
       if (serverMemberId) {
         webviewBridge(webviewRef, 'login', {
           token: user?.token,
@@ -142,15 +169,22 @@ const App = () => {
       }
       setLoading(false);
     }
-    if (event.nativeEvent.data === 'notification') {
+    if (data.message === 'notification') {
       requestUserPermission();
     }
-    if (event.nativeEvent.data === 'goBack') {
+    if (data.message === 'goBack') {
       captureScreenFn(setSnapshotUri);
       setIsGoingBack(true);
     }
-    if (event.nativeEvent.data === 'signUp') {
+    if (data.message === 'signUp') {
       auth().signOut();
+    }
+    setNoAnimation(false);
+    if (data.message === 'visitBookmark') {
+      setWebviewMode('BOOKMARK');
+      setNoAnimation(true);
+      setUrl(data.params?.url ?? '');
+      navigationRef.current?.isReady && router.push('webview');
     }
   };
 
@@ -184,7 +218,7 @@ const App = () => {
                 requestAnimationFrame(() => {
                   Animated.timing(imageOpacity, {
                     toValue: 1,
-                    duration: 300,
+                    duration: 250,
                     useNativeDriver: true,
                     easing: Easing.linear,
                   }).start(() => {
@@ -228,7 +262,7 @@ const App = () => {
                 webviewRef.current?.reload();
                 setTimeout(() => {
                   setLoading(false);
-                }, 3000);
+                }, 2500);
               }}
             />
           </SafeAreaView>
