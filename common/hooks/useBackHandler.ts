429529929b0706a5d7b08780dc8a299eb3a70056
ww8007 <wshmin1234@gmail.com>
2023-08-26 10:13:01 +0900

    feat: 안드로이드 뒤로가기 동작을 위한 custom hook 추가

---
+++ common/hooks/useBackHandler.ts
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import { BackHandler } from 'react-native';
+
+export const useBackHandler = (handleBackAction: () => boolean) => {
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      handleBackAction,
+    );
+
+    return () => backHandler.remove();
+  }, []);
+};
